78b62098e7c533f3014a76d7cc8b59ff7a423ecc
Alexander <alexander.freeman77@gmail.com>
2017-07-21 00:36:26 +0300

    Added performance tests for determinant and inverse methods

--- test_numpy_compare.py
+++ test_numpy_compare.py
@@ -1,6 +1,7 @@
 import numpy as np
 import unittest
 import matrices
+import time
 
 
 class CustomTests(unittest.TestCase):
@@ -161,3 +162,181 @@ class CustomTests(unittest.TestCase):
         # AssertionError: 4029.9999999999991 != Decimal('4030.00'
         # self.assertEqual(np.linalg.det(numG), matG.matrix_determinant())
         self.assertAlmostEqual(np.linalg.det(numG), float(matG.matrix_determinant()))
+
+    def test_3(self):
+        """ Here we will run a few performance checks for 'matrices.py' and 'numpy'
+            First, we'll check speed of determinant calculation, than - inverse matrix computation
+            This is not a real Unittest and lots of duplicated code can be bad for yor health!
+            Short conclusion: our methods way faster from numpy for a 2x2 example matrix"""
+        det_cycles = 1
+        numG = np.array(self.matrix_g)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = np.linalg.det(numG)
+        end_time = time.perf_counter()
+        total_time_num = end_time - start_time
+        matG = matrices.Matrix(self.matrix_g)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = matG.matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat = end_time - start_time
+        print("="*70)
+        print("For matrix G(7x7) calculation of determinant {0} time(s) by numpy took {1} seconds".format(
+                                                det_cycles, total_time_num))
+        print("For matrix G(7x7) calculation of determinant {0} time(s) by matrices.py took {1} seconds".format(
+                                                det_cycles, total_time_mat))
+        # as a result we have this:
+        # For matrix G(7x7) calculation of determinant 100000 time(s) by numpy took 2.5861989099275493 seconds
+        # For matrix G(7x7) calculation of determinant 100000 time(s) by matrices.py took 21.711577452218382 seconds
+        # For matrix G(7x7) calculation of determinant 100 time(s) by numpy took 0.002622916097404813 seconds
+        # For matrix G(7x7) calculation of determinant 100 time(s) by matrices.py took 0.02498621645262421 seconds
+        # As expected: numpy with his progressive algorithms calculate determinant much faster than our teaching method
+
+        # Now, lets check if its true for 2x2 matrix
+        # (and yes - we copied previous code, and that, so you already know - is a bad programing example):
+        det_cycles = 1
+        numA = np.array(self.matrix_a)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = np.linalg.det(numA)
+        end_time = time.perf_counter()
+        total_time_num = end_time - start_time
+        matA = matrices.Matrix(self.matrix_a)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = matA.matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat = end_time - start_time
+        print("="*70)
+        print("For matrix A(2x2) calculation of determinant {0} time(s) by numpy took {1} seconds".format(
+            det_cycles, total_time_num))
+        print("For matrix A(2x2) calculation of determinant {0} time(s) by matrices.py took {1} seconds".format(
+            det_cycles, total_time_mat))
+        # WE, actually, was faster this time(in our method 2x2 matrices was calculating by using standard formula)
+        # For matrix A(2x2) calculation of determinant 100 time(s) by numpy took 0.0022251738505491593 seconds
+        # For matrix A(2x2) calculation of determinant 100 time(s) by matrices.py took 0.0008682422217946573 seconds
+        # For matrix A(2x2) calculation of determinant 100000 time(s) by numpy took 2.4833566697009335 seconds
+        # For matrix A(2x2) calculation of determinant 100000 time(s) by matrices.py took 0.8371969034310927 seconds
+
+        # Finally, lets check 3x3 matrices. We, also, will use _matrix_determinant - a private method of 'matrices.py'
+        # where for computing determinant static formula was used:
+        det_cycles = 1
+        numB = np.array(self.matrix_b)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = np.linalg.det(numB)
+        end_time = time.perf_counter()
+        total_time_num = end_time - start_time
+        matB = matrices.Matrix(self.matrix_b)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = matB.matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat = end_time - start_time
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = matB._matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat_prive = end_time - start_time
+        print("="*70)
+        print("For matrix B(3x3) calculation of determinant {0} time(s) by numpy took {1} seconds".format(
+            det_cycles, total_time_num))
+        print("For matrix B(3x3) calculation of determinant {0} time(s) by matrices.py took {1} seconds".format(
+            det_cycles, total_time_mat))
+        print("For matrix B(3x3) calculation of determinant {0} time(s) by matrices.py using static formula"
+              " took {1} seconds".format(det_cycles, total_time_mat_prive))
+        # here the results:
+        # For matrix B(3x3) calculation of determinant 100 time(s) by numpy took 0.002191624453710725 seconds
+        # For matrix B(3x3) calculation of determinant 100 time(s) by matrices.py took 0.006737769830118791 seconds
+        # For matrix B(3x3) calculation of determinant 100 time(s) by matrices.py using static formula took 0.0006960489319973936 seconds
+        # For matrix B(3x3) calculation of determinant 100000 time(s) by numpy took 2.5949706624671984 seconds
+        # For matrix B(3x3) calculation of determinant 100000 time(s) by matrices.py took 3.7783217540756455 seconds
+        # For matrix B(3x3) calculation of determinant 100000 time(s) by matrices.py using static formula took 0.705146881684616 seconds
+        # AS we see: numpy calculate 'slightly' faster than our general method, but our private mrthod for 3x3 matrices much faster!
+
+        # Now, how about linearly dependent matrix:
+        det_cycles = 1
+        numI = np.array(self.matrix_i)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = np.linalg.det(numI)
+        end_time = time.perf_counter()
+        total_time_num = end_time - start_time
+        matI = matrices.Matrix(self.matrix_i)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = matI.matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat = end_time - start_time
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            det = matI._matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat_prive = end_time - start_time
+        print("="*70)
+        print("For linearly dependent matrix I(3x3) calculation of determinant {0} time(s) by numpy took {1} seconds".format(
+            det_cycles, total_time_num))
+        print("For linearly dependent matrix I(3x3) calculation of determinant {0} time(s) by matrices.py took {1} seconds".format(
+            det_cycles, total_time_mat))
+        print("For linearly dependent matrix I(3x3) calculation of determinant {0} time(s) by matrices.py using static formula"
+              " took {1} seconds".format(det_cycles, total_time_mat_prive))
+        # For linearly dependent matrix I(3x3) calculation of determinant 100 time(s) by numpy took 0.002870292372888207 seconds
+        # For linearly dependent matrix I(3x3) calculation of determinant 100 time(s) by matrices.py took 0.00395559515013153 seconds
+        # For linearly dependent matrix I(3x3) calculation of determinant 100 time(s) by matrices.py using static formula took 0.0006713921463691467 seconds
+        # For linearly dependent matrix I(3x3) calculation of determinant 100000 time(s) by numpy took 2.4001016182935566 seconds
+        # For linearly dependent matrix I(3x3) calculation of determinant 100000 time(s) by matrices.py took 2.7887345975931743 seconds
+        # For linearly dependent matrix I(3x3) calculation of determinant 100000 time(s) by matrices.py using static formula took 0.6946370278629761 seconds
+        # Yeh, for 100000 iterations numpy was just a bit faster, but this difference grown to almost 5 second
+        # for  1000000 iterations, in the other hand - static formula in our private method still working faster
+
+        # Now, lets test some inversion methods:
+        # Attention: duplicated code! Try not to do this in your programs - construct a method, which use a duplicates
+        det_cycles = 1
+        numG = np.array(self.matrix_g)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            numGinv = np.linalg.inv(numG)
+        end_time = time.perf_counter()
+        total_time_num = end_time - start_time
+        matG = matrices.Matrix(self.matrix_g)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            matGinv = matG.matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat = end_time - start_time
+        print("="*70)
+        print("For matrix G(7x7) computation of inverse matrix {0} time(s) by numpy took {1} seconds".format(
+            det_cycles, total_time_num))
+        print("For matrix G(7x7) computation of inverse matrix {0} time(s) by matrices.py took {1} seconds".format(
+            det_cycles, total_time_mat))
+        # For matrix G(7x7) computation of inverse matrix 100 time(s) by numpy took 0.0031977021492632877 seconds
+        # For matrix G(7x7) computation of inverse matrix 100 time(s) by matrices.py took 0.030527930074972794 seconds
+        # For matrix G(7x7) computation of inverse matrix 100000 time(s) by numpy took 2.9690120793997004 seconds
+        # For matrix G(7x7) computation of inverse matrix 100000 time(s) by matrices.py took 22.447151615787458 seconds
+        # Ok, numpy won this with great margin, but... the thing is: we calculated determinant in our inverse method
+        # and it took 21.71 second for previous test to do that. So, actual, the inverting procedure
+        # was less, than a second(anyway - it's not count without determinant check)
+        det_cycles = 100000
+        numA = np.array(self.matrix_a)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            numAinv = np.linalg.inv(numA)
+        end_time = time.perf_counter()
+        total_time_num = end_time - start_time
+        matA = matrices.Matrix(self.matrix_a)
+        start_time = time.perf_counter()
+        for i in range(1, det_cycles):
+            matAinv = matA.matrix_determinant()
+        end_time = time.perf_counter()
+        total_time_mat = end_time - start_time
+        print("="*70)
+        print("For matrix A(2x2) computation of inverse matrix {0} time(s) by numpy took {1} seconds".format(
+            det_cycles, total_time_num))
+        print("For matrix A(2x2) computation of inverse matrix {0} time(s) by matrices.py took {1} seconds".format(
+            det_cycles, total_time_mat))
+        # For matrix A(2x2) computation of inverse matrix 100 time(s) by numpy took 0.004653260920530776 seconds
+        # For matrix A(2x2) computation of inverse matrix 100 time(s) by matrices.py took 0.0009753577659173699 seconds
+        # For matrix A(2x2) computation of inverse matrix 100000 time(s) by numpy took 2.756857820081456 seconds
+        # For matrix A(2x2) computation of inverse matrix 100000 time(s) by matrices.py took 0.8733635574325254 seconds
+        # Yes, we small matrix(2x2) our method way faster!
